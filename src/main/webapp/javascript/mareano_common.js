f775c70f1190a2064d4f26c5cfc363cfd657da67
Bart van den Eijnden <bartvde@boundlessgeo.com>
2013-12-23 13:45:30 +0100

    make sure we have proper i18n for the Mareano language strings

--- src/main/webapp/javascript/mareano_common.js
+++ src/main/webapp/javascript/mareano_common.js
@@ -8,44 +8,6 @@ Ext.ns("Mareano");
 
 Mareano.Composer = Ext.extend(GeoExplorer.Composer, {
 
-    // Begin i18n.
-    zoomSliderText: "<div>Zoom Level: {zoom}</div><div>Scale: 1:{scale}</div>",
-    loadConfigErrorText: "Trouble reading saved configuration: <br />",
-    loadConfigErrorDefaultText: "Server Error.",
-    xhrTroubleText: "Communication Trouble: Status ",
-    layersText: "Kartlag",
-    titleText: "Title",
-    saveErrorText: "Trouble saving: ",
-    bookmarkText: "Bookmark URL",
-    permakinkText: 'Permalink',
-    appInfoText: "Mareano",
-    aboutText: "About GeoExplorer",
-    mapInfoText: "Map Info",
-    descriptionText: "Description",
-    contactText: "Contact",
-    aboutThisMapText: "About this Map",
-    thematicText: "Temakart",
-    dataSuppliedText: "Data levert av:",
-    legendTitle: "Tegnforklaring",
-    infoTitle: "Info om kartlag",
-    helpTitle: "Hjelp",
-    zoomBoxTooltip: "Zoom til omr\u00e5de",
-    mousePositionText: "Koordinater (WGS84): ",
-    goToTooltip: "G&aring; til koordinat",
-    goToText: "G&aring; til koordinat",
-    goToPrompt: "Posisjon i WGS84 (Breddegrad, Lengdegrad - for eksempel: 60.2,1.5):",
-    zoomToItem1: "Barents Sea",
-    zoomToItem2: "Norwegian Sea",
-    zoomToItem3: "North Sea",
-    zoomToItem4: "Skagerrak",
-    zoomToItem5: "Polhavet",
-    expandText: "Expand Layers",
-    collapseText: "Collapse Layers",
-    expandCollapseTooltip: "Expand or collapse the Layers panel",
-    visibilityText: "Turn off",
-    visibilityTooltip: "Turn off all overlays",
-    // End i18n.
-
     constructor: function() {
         Mareano.Composer.superclass.constructor.apply(this, arguments);    
         this.on("beforecreateportal", this.modifyPortal, this);
@@ -402,7 +364,55 @@ Mareano.Composer = Ext.extend(GeoExplorer.Composer, {
 
 });
 
-// end of GeoExplorer overrides
+/* TODO strings with * need translation */
+
+GeoExt.Lang.add("no", {
+    "Mareano.Composer.prototype": {
+        thematicText: "Temakart",
+        legendTitle: "Tegnforklaring",
+        infoTitle: "Info om kartlag",
+        helpTitle: "Hjelp",
+        helpText: "For \u00e5 gj\u00f8re det enklere \u00e5 navigere og endre p\u00e5 hva kartene viser, er det laget to ulike paneler for \u00e5 styre dette. I &#34;Temakart&#34; til venstre ligger alle ferdige kart og kartlag som man kan velge \u00e5 sl\u00e5 p\u00e5. De er organisert i hovedtema, under hovedtema er kartbilder som man kan velge \u00e5 sl\u00e5 av/p\u00e5, og p\u00e5 det nederste niv\u00e5et er kartlag som man ogs\u00e5 kan sl\u00e5s av/p\u00e5. N\u00e5r man \u00e5pner et temakart, blir et p\u00e5 forh\u00e5nd definert utvalg av kartlag \u00e5pnet. Disse kartlagene blir synlige under &#34;Kartlag&#34; i det h\u00f8yre kartpanelet. Her kan rekkef\u00f8lgen p\u00e5 kartlagene endres ved \u00e5 flytte p\u00e5 de ulike kartlagene, dette kan p\u00e5 enkelte kartlag for eksempel brukes til \u00e5 velge hvilke punkt som skal v\u00E6re mest synlige. Man kan ogs\u00e5 h\u00f8yreklikke p\u00e5 tittelen p\u00e5 hvert kartlag for \u00e5 bl.a. zoome til kartlagsutstrekning, det kan v\u00E6re nyttig for kartlag som har innhold som er mer synlig viss man zoomer inn p\u00e5 dem. For sp\u00f8rsm\u00e5l send mail til <a href='gis@nmd.no'>gis@nmd.no</a>",
+        mousePositionText: "Koordinater (WGS84): ",
+        goToTooltip: "G&aring; til koordinat",
+        goToText: "G&aring; til koordinat",
+        goToPrompt: "Posisjon i WGS84 (Breddegrad, Lengdegrad - for eksempel: 60.2,1.5):",
+        zoomToItem1: "Barentshavet",
+        zoomToItem2: "Norskehavet",
+        zoomToItem3: "Nordsj\u00f8en",
+        zoomToItem4: "Skagerrak",
+        zoomToItem5: "Polhavet",
+        expandText: "* Expand Layers *",
+        collapseText: "* Collapse Layers *",
+        expandCollapseTooltip: "* Expand or collapse the Layers panel *",
+        visibilityText: "* Turn off *",
+        visibilityTooltip: "* Turn off all overlays *"
+    }
+});
+
+GeoExt.Lang.add("en", {
+    "Mareano.Composer.prototype": {
+        thematicText: "Thematic maps",
+        legendTitle: "Legend",
+        infoTitle: "Info about layers",
+        helpTitle: "Help",
+        helpText: "",
+        mousePositionText: "Coordinates (WGS84): ",
+        goToTooltip: "Go to coordinate",
+        goToText: "Go to coordinate",
+        goToPrompt: "Position in WGS84 (Latitude, Longitude - eg. - 60.2,1.5):",
+        zoomToItem1: "Barents Sea",
+        zoomToItem2: "Norwegian Sea",
+        zoomToItem3: "North Sea",
+        zoomToItem4: "Skagerrak",
+        zoomToItem5: "Polhavet",
+        expandText: "Expand Layers",
+        collapseText: "Collapse Layers",
+        expandCollapseTooltip: "Expand or collapse the Layers panel",
+        visibilityText: "Turn off",
+        visibilityTooltip: "Turn off all overlays"
+    }
+});
 
 var silent = false;
 var kartlagInfoState = ""; //used by removeLayerLegendAndInfo(mapOfGMLspesialpunkt, kartlagId)
