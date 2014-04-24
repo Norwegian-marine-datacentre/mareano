9af4883e331c4a45c6aef8a0abf9cf8643ba9d91
emoen <endremoen@hotmail.com>
2014-04-24 11:36:17 +0200

    bugfix: #127 removed too much in commit: ea6efbc added function to remove Legend graphics.

--- src/main/webapp/javascript/mareano_common.js
+++ src/main/webapp/javascript/mareano_common.js
@@ -445,6 +445,10 @@ function removeLayerLegendAndInfo(mapOfGMLspesialpunkt, kartlagId, record, layer
     	app.mapPanel.map.removeLayer(mapOfGMLspesialpunkt[kartlagId], false);
     	mapOfGMLspesialpunkt[kartlagId] = null;
     }
+	
+    var legendDiv = '#'+kartlagId; //fjern legend
+    jQuery(legendDiv).remove();
+	
     fjernKartlagInfo(kartlagId);
 }
 
