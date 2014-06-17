117689fd15b4cced16b9cfabecca8c8a9b485d01
emoen <endremoen@hotmail.com>
2014-06-17 11:39:39 +0200

    updated google-analytics script

--- src/main/webapp/javascript/googleAnalyticsStatistics.js
+++ src/main/webapp/javascript/googleAnalyticsStatistics.js
@@ -1,7 +1,7 @@
 var _gaq = _gaq || [];
-_gaq.push(['_setAccount', 'UA-7675964-2']);
+_gaq.push(['_setAccount', 'UA-390810-4']);
+_gaq.push (['_gat._anonymizeIp']);
 _gaq.push(['_trackPageview']);
-
 (function() {
   var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
   ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
